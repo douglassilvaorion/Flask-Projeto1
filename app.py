bc20f96c429f63ed4b159c34f1b6dab7de5ab390
Douglas Silva <douglas.silva@orionconsult.com.br>
2024-01-11 08:04:45 -0300

    Mensagens+API+BD

--- app.py
+++ app.py
@@ -108,7 +108,53 @@ def contas():
 
 @app.route('/mensagens', methods=["GET", "POST"])
 def mensagens():
-	return render_template("mensagens.html", accounts=accounts.query.all())
+
+	url = "https://wapi.autotrac-online.com.br/sandboxaticapi/v1/accounts/11/vehicles/651/returnmessages"
+	payload = {}
+	files={}
+	headers = {
+  	'Authorization': 'Basic c3Vwb3J0ZUBhbWF6b246anVlekAyMDE3',
+  	'Cookie': 'TS01f4576b=01325e1fda33a063404ba6946937f1da03f1aea65fd080a1c7db1dd57cfb82803618df4f926084704c6324aa4fa124398d0a056acc'}
+
+	response = requests.request("GET", url, headers=headers, data=payload, files=files)
+	objetos    = json.loads(response.text)
+	dados      = objetos['Data']
+
+	df = pd.DataFrame(dados)
+
+	for col in df.columns:
+		df[col] = df[col].apply(str)
+
+	for i in df.index:
+		if messages.query.filter_by(id=df['ID'][i]).first():
+			print('Registro já existe')
+		else:
+			menssage = messages(df['ID'][i],
+						df['AccountNumber'][i],
+						df['VehicleAddress'][i],
+						df['Priority'][i],
+						df['Grmn'][i],
+						df['Ignition'][i],
+						df['MacroNumber'][i],
+						df['MacroVersion'][i],
+						df['BinaryDataType'][i],
+						df['MsgSubType'][i],
+						df['MessageTime'][i],
+						df['MessageText'][i],
+						df['Latitude'][i],
+						df['Longitude'][i],
+						df['PositionTime'][i],
+						df['Landmark'][i],
+						df['TransmissionChannel'][i])
+			db.session.add(menssage)
+			db.session.commit()
+
+			#Recurso de Paginação
+			# page = request.args.get('page', 1, type=int)
+			# per_page = 4
+			# #todos_messages = messages.query.paginate(page, per_page)
+
+	return render_template("mensagens.html", messages=messages.query.all())
 
 if __name__ =="__main__":
 	db.create_all()
