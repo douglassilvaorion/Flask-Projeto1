36ea6d056be33019e693d94951741a90db242b60
Douglas Silva <douglas.silva@orionconsult.com.br>
2024-02-08 15:12:48 -0300

    ajuste para consulta de posição

--- app.py
+++ app.py
@@ -146,8 +146,24 @@ def macros_add():
 @app.route('/veiculos_posicao',methods=["GET","POST"])
 def veiculos_posicao():
 
-	return render_template('/veiculos_posicao.html', posicao = vehiclespositions.query.all())
+	code_vehicles = request.args.get('code')
 
+	url = "https://aapi3.autotrac-online.com.br/aticapi/v1/accounts/11035/vehicles/"+code_vehicles+"/positions"
+
+	payload = {}
+	files={}
+	headers = { 'Authorization': 'Basic suporte@amazon:juez@2017',  'Ocp-Apim-Subscription-Key': '011cb03f29064101858f71356ac6f6e5', 'Content-Type': 'application/json' }
+
+	response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+	objetos    = json.loads(response.text)
+	
+	if (response.status_code) == 200:
+		dados      = objetos['Data']
+		return render_template('/veiculos_posicao.html', len = len(dados), posicao = dados)
+	else:
+		return redirect(url_for('non_date_vehicle'))
+	
 @app.route('/non_actorized_vehicle', methods=["GET","POST"])
 def non_actorized_vehicle():
     return render_template("non_actorized_vehicle.html")
